578af2ce3246c5686e26ce5fe42ac443155e59dc
Alisa Hung <alisa.hung@ailabs.tw>
2021-06-16 22:53:08 +0800

    Widget - Add widget

---
+++ SocialDistance/src/main/java/tw/gov/cdc/exposurenotifications/widget/AppWidgetProvider.kt
@@ -0,0 +1,41 @@
+package tw.gov.cdc.exposurenotifications.widget
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import tw.gov.cdc.exposurenotifications.R
+import tw.gov.cdc.exposurenotifications.activity.BarcodeScanningActivity
+
+class AppWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        appWidgetIds.forEach { appWidgetId ->
+            // Create an Intent to launch ExampleActivity
+            val pendingIntent: PendingIntent = Intent(context, BarcodeScanningActivity::class.java)
+                .let { intent ->
+                    PendingIntent.getActivity(context, 0, intent, 0)
+                }
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            val views: RemoteViews = RemoteViews(
+                context.packageName,
+                R.layout.app_widget
+            ).apply {
+                setOnClickPendingIntent(R.id.widget_barcode_button, pendingIntent)
+                setOnClickPendingIntent(R.id.widget_barcode_text, pendingIntent)
+            }
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
