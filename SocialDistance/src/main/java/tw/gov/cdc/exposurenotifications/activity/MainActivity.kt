f90968c371759dfdc4315051c9fc3274b3768d2c
Alisa Hung <alisa.hung@ailabs.tw>
2021-07-07 16:57:27 +0800

    Feature Hints - Update UI for `Feature.BARCODE_V2`

--- SocialDistance/src/main/java/tw/gov/cdc/exposurenotifications/activity/MainActivity.kt
+++ SocialDistance/src/main/java/tw/gov/cdc/exposurenotifications/activity/MainActivity.kt
@@ -10,8 +10,9 @@ import android.os.Bundle
 import android.os.Looper
 import android.text.Spannable
 import android.text.SpannableStringBuilder
+import android.text.method.LinkMovementMethod
 import android.text.method.ScrollingMovementMethod
-import android.text.style.StyleSpan
+import android.text.style.*
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -26,12 +27,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 import tw.gov.cdc.exposurenotifications.BuildConfig
 import tw.gov.cdc.exposurenotifications.R
 import tw.gov.cdc.exposurenotifications.common.*
+import tw.gov.cdc.exposurenotifications.common.BulletSpan
 import tw.gov.cdc.exposurenotifications.common.FeaturePresentManager.Feature
 import tw.gov.cdc.exposurenotifications.common.Utils.getDateString
 import tw.gov.cdc.exposurenotifications.nearby.ExposureNotificationManager
 import tw.gov.cdc.exposurenotifications.nearby.ExposureNotificationManager.ExposureNotificationState
 import java.lang.reflect.InvocationTargetException
 import java.util.*
+
 
 class MainActivity : BaseActivity() {
 
@@ -74,6 +77,11 @@ class MainActivity : BaseActivity() {
     private val allFeatures = mutableListOf(Feature.BARCODE_V2, Feature.DAILY_SUMMARY)
     private var currentPresentingFeature: Feature? = null
     private val featureBarcodeGroup by lazy { feature_barcode_group }
+    private val featureBarcodeTipText by lazy {
+        feature_barcode_tip_text.apply {
+            movementMethod = LinkMovementMethod()
+        }
+    }
     private val featureDailySummaryGroup by lazy { feature_daily_summary_group }
     private val featureTouchView by lazy { feature_touch_view }
 
@@ -116,9 +124,7 @@ class MainActivity : BaseActivity() {
 
         textVersion.text = "v${BuildConfig.VERSION_NAME}"
 
-        featureTouchView.setOnClickListener {
-            clearCurrentPresentingFeature()
-        }
+        setupFeatureUI()
     }
 
     override fun onStart() {
@@ -526,6 +532,92 @@ class MainActivity : BaseActivity() {
         }
     }
 
+    private fun setupFeatureUI() {
+        featureTouchView.setOnClickListener {
+            clearCurrentPresentingFeature()
+        }
+
+        featureBarcodeTipText.apply {
+
+            var lastFeatureBarcodeScrollTime = Date().time
+
+            setOnScrollChangeListener { _, _, _, _, _ ->
+                lastFeatureBarcodeScrollTime = Date().time
+            }
+
+            setOnClickListener {
+                if (Date().time - lastFeatureBarcodeScrollTime > 300) {
+                    clearCurrentPresentingFeature()
+                }
+            }
+
+            text = SpannableStringBuilder().apply {
+                val barcodeTipStrings = resources.getStringArray(R.array.feature_barcode_tip)
+                val docString = getString(R.string.feature_barcode_tip_doc)
+                val splitMark = getString(R.string.feature_barcode_tip_split)
+
+                var startIndex = 0
+
+                barcodeTipStrings.forEachIndexed { index, string ->
+                    append(string)
+
+                    val docStringIndex = string.indexOf(docString)
+                    if (docStringIndex > 0) {
+                        setSpan(
+                            URLSpan(getString(R.string.url_quick_settings)),
+                            startIndex + docStringIndex,
+                            startIndex + docStringIndex + docString.length,
+                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                        )
+                    }
+
+                    when (index) {
+                        2, 4 -> {
+                            setSpan(
+                                RelativeSizeSpan(1.1f),
+                                startIndex,
+                                lastIndex + 1,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                            setSpan(
+                                LeadingMarginSpan.Standard(50, 110),
+                                startIndex,
+                                lastIndex + 1,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                        }
+                        else -> {
+                            val splitMarkIndex = string.indexOf(splitMark)
+                            setSpan(
+                                StyleSpan(Typeface.BOLD),
+                                startIndex,
+                                startIndex + splitMarkIndex + splitMark.length,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                            setSpan(
+                                RelativeSizeSpan(1.3f),
+                                startIndex,
+                                startIndex + splitMarkIndex + splitMark.length,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                        }
+                    }
+
+                    if (index < 4) {
+                        val proportion = if (index == 0) 0.4f else 1.0f
+                        append(
+                            "\n",
+                            RelativeSizeSpan(proportion),
+                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                        )
+                    }
+
+                    startIndex = lastIndex + 1
+                }
+            }
+        }
+    }
+
     private fun presentFeatureIfNeeded() {
         if (currentPresentingFeature == null) {
             FeaturePresentManager.getFeaturesNeedToPresent(allFeatures).forEach {
@@ -540,8 +632,10 @@ class MainActivity : BaseActivity() {
     private fun presentFeature(feature: Feature): Boolean {
         return when (feature) {
             Feature.BARCODE_V2 -> {
+                featureBarcodeTipText.scrollTo(0, 0)
                 featureBarcodeGroup.visibility = View.VISIBLE
-                featureTouchView.visibility = View.VISIBLE
+                // Detect touch in `featureBarcodeTipText`
+                // featureTouchView.visibility = View.VISIBLE
                 true
             }
             Feature.DAILY_SUMMARY -> {
